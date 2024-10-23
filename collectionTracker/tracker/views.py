5da27fa7b6cf351835f722a9396dd58afa758f92
Reidy <janine_reidenbach@fom-net.de>
2024-10-23 14:39:03 +0200

    edit test message

--- collectionTracker/tracker/views.py
+++ collectionTracker/tracker/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Möge der Code immer mit uns sein! Willkommen Janine, Marlon und Marius 👩‍💻✨")
+    return HttpResponse("Möge der Code immer mit uns sein! Willkommen Janine, Marlon und Marius 👩‍💻✨ Test-Ballon")
