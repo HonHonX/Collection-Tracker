13cfff66ec3c0fe6556c17f1e6fea8c7da13837c
HonHonX <buch.sharon@googlemail.com>
2024-10-28 22:20:29 +0100

    change layout and components in html, views and css

--- collectionTracker/tracker/views.py
+++ collectionTracker/tracker/views.py
@@ -59,7 +59,10 @@ def index(request):
 def artist_search(request):
     albums = []
     error = None
-    
+    artist_photo_url = None
+    artist_info = {} 
+    latest_album = None
+
     if request.method == 'POST':
         artist_name = request.POST.get('artist_name')
         
@@ -67,32 +70,62 @@ def artist_search(request):
             # Search by name → Spotify API
             result = sp.search(q=artist_name, type='artist')
             
-            if len(result['artists']['items']) == 0: #List empty
+            if len(result['artists']['items']) == 0:  # List empty
                 error = f"Artist {artist_name} not found!"
             else:
-                # Get artist's Spotify ID
-                artist_id = result['artists']['items'][0]['id']
-                
+                # Get artist's Spotify ID and image
+                artist_info = result['artists']['items'][0]
+                artist_id = artist_info['id']
+                artist_photo_url = artist_info['images'][0]['url'] if artist_info['images'] else None
+
                 # Get all albums for the artist using the artist ID
-                album_results = sp.artist_albums(artist_id=artist_id, album_type='album, single')
+                album_results = sp.artist_albums(artist_id=artist_id)
                 
-                for album in album_results['items']:
+                # Sort albums by release date
+                sorted_albums = sorted(
+                    album_results['items'],
+                    key=lambda x: x['release_date'],
+                    reverse=True  # Set to True if you want latest albums first
+                )
+
+                # Count total number of albums
+                total_albums = len(sorted_albums)
+
+                for album in sorted_albums:
                     # Fetch album details including the cover image
                     albums.append({
                         'name': album['name'],
                         'release_date': album['release_date'],
                         'total_tracks': album['total_tracks'],
+                        'album_type': album['album_type'], 
                         'url': album['external_urls']['spotify'],
                         'image_url': album['images'][0]['url'] if album['images'] else None,
                         'spotify_id': album['id'],
                     })
-        except Exception as e:
-            error = str(e)# Redirect to artist_overview page, passing data as context
 
+                # Add artist info to the context (you can choose which fields to include)
+                artist_info = {
+                    'name': artist_info['name'],
+                    'genres': artist_info['genres'],  # List of genres
+                    'popularity': artist_info['popularity'],  # Popularity score
+                    'total_albums': total_albums  # Total number of albums
+                }
+
+                # The first album in the sorted list is the latest
+                if sorted_albums:
+                    latest_album = sorted_albums[0]
+
+        except Exception as e:
+            error = str(e)  # Capture error message
+
+        # Pass albums, artist info, artist photo, and error message to the template
         return render(request, 'tracker/artist_overview.html', {
             'albums': albums,
-            'artist_name': artist_name,
+            'artist_name': artist_info['name'],
+            'artist_photo_url': artist_photo_url,
             'error': error,
+            'artist_info': artist_info,
+            'latest_album': latest_album,
         })
     
     return render(request, 'tracker/artist_search.html', {
