a4e58ab643551d9501c60067beadea6699d9b331
Reidy <janine_reidenbach@fom-net.de>
2024-11-03 12:44:32 +0100

    add LoginRequiredMixin to protect internal sites

--- collectionTracker/home/views.py
+++ collectionTracker/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -8,11 +9,11 @@ from django.conf import settings
 # running in various configurations
 
 
-class HomeView(View):
+class HomeView(LoginRequiredMixin, View):
     def get(self, request):
-        print(request.get_host())
-        host = request.get_host()
-        islocal = host.find('localhost') >= 0 or host.find('127.0.0.1') >= 0
+        #print(request.get_host())
+        #host = request.get_host()
+        #islocal = host.find('localhost') >= 0 or host.find('127.0.0.1') >= 0
         #context = {
         #    'installed': settings.INSTALLED_APPS,
         #    'islocal': islocal
