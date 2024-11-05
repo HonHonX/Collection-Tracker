303e4439160d1755c944508b3e24b38fde9657ae
Reidy <119943084+JanineReidenbach@users.noreply.github.com>
2024-11-05 14:40:24 +0100

    Accountmanagement (#25)
    * add login and register
    
    * add social login with github
    
    * add LoginRequiredMixin to protect internal sites
    
    * fix sociallogin for pythonanywhere, localhost doesn´t work yet
    
    * fix sociallogin for pythonanywhere, localhost doesn´t work yet (2nd try)
    
    ---------
    
    Co-authored-by: Reidy <janine_reidenbach@fom-net.de>

--- collectionTracker/collectionTracker/github_settings.py
+++ collectionTracker/collectionTracker/github_settings.py
@@ -9,10 +9,10 @@
 
 # Using PythonAnywhere here are some settings:
 
-# Application name: ChuckList PythonAnywhere
-# Homepage Url: https://drchuck.pythonanywhere.com
-# Application Description: Whatever
-# Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
+# Application name: CollectionTracker PythonAnywhere
+# Homepage Url: https://wtcollectiontracker.eu.pythonanywhere.com
+# Application Description: Developing a web application
+# Authorization callback URL: https://wtcollectiontracker.eu.pythonanywhere.com/oauth/complete/github/
 
 # Also on PythonAnywhere, go into the Web tab and enable "Force HTTPS"
 # so you don't get a redirect URI mismatch.
@@ -20,27 +20,31 @@
 # Then copy the client_key and secret to this file
 
 from decouple import config
-import socket
+##import socket
+## fixed for pythonanywhere:
+# the hostname cannot be called reliably via pythonanywhere and github, 
+# although the hostname is correct, 
+# the "else" branch is chosen and the data is used for localhost
 
 # Determine the hostname
-hostname = socket.gethostname()
+##hostname = socket.gethostname()
 
 # Check if you're on localhost or production based on the hostname or environment variable
-if hostname == 'wtcollectiontracker.eu.pythonanywhere.com':
+##if hostname == 'wtcollectiontracker.eu.pythonanywhere.com':
 
-    SOCIAL_AUTH_GITHUB_KEY = config('SOCIAL_AUTH_GITHUB_KEY_PyA')
-    SOCIAL_AUTH_GITHUB_SECRET = config('SOCIAL_AUTH_GITHUB_KEY_PyA')
+SOCIAL_AUTH_GITHUB_KEY = config('SOCIAL_AUTH_GITHUB_KEY_PyA')
+SOCIAL_AUTH_GITHUB_SECRET = config('SOCIAL_AUTH_GITHUB_SECRET_PyA')
 
-else:
+##else:
     # If you are running on localhost, here are some settings:
 
-    # Application name: ChuckList Local
+    # Application name: Collectiontracker Localhost
     # Homepage Url: http://localhost:8000
-    # Application Description: Whatever
+    # Application Description: Developing a web application
     # Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
 
-    SOCIAL_AUTH_GITHUB_KEY = config('SOCIAL_AUTH_GITHUB_KEY_LOCAL')
-    SOCIAL_AUTH_GITHUB_SECRET = config('SOCIAL_AUTH_GITHUB_KEY_LOCAL')
+##    SOCIAL_AUTH_GITHUB_KEY = config('SOCIAL_AUTH_GITHUB_KEY_LOCAL')
+##    SOCIAL_AUTH_GITHUB_SECRET = config('SOCIAL_AUTH_GITHUB_SECRET_LOCAL')
 
 # Ask for the user's email (don't edit this line)
 SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
