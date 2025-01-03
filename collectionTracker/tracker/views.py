ade8b381a821d5619e2195ffc93d8a18b52db504
Sherry <119943082+HonHonX@users.noreply.github.com>
2025-01-03 16:21:14 +0100

    implement index

--- collectionTracker/tracker/views.py
+++ collectionTracker/tracker/views.py
@@ -10,6 +10,7 @@ import logging
 from django.contrib.auth.decorators import login_required
 from collection.models import Artist, Album, UserAlbumCollection, UserAlbumWishlist, UserAlbumBlacklist, UserProgress, UserArtistProgress, UserFollowedArtists  # Import the model
 import json
+from django.db import transaction
 
 # Configure logger
 logger = logging.getLogger(__name__)
@@ -53,7 +54,6 @@ def artist_search(request):
             if len(result['artists']['items']) == 0:  # List empty
                 error = f"Artist {artist_name} not found!"
             else:
-                # Get artist's Spotify ID and image
                 artist_info = result['artists']['items'][0]
                 artist_id = artist_info['id']
                 artist_photo_url = artist_info['images'][0]['url'] if artist_info['images'] else None
@@ -126,6 +126,30 @@ def artist_search(request):
                     collection_count = sum(1 for album in sorted_albums if album['id'] in user_album_ids)
                     wishlist_count = sum(1 for album in sorted_albums if album['id'] in user_wishlist_ids)
                     blacklist_count = sum(1 for album in sorted_albums if album['id'] in user_blacklist_ids)
+
+                # Save artist and albums to the database
+                with transaction.atomic():
+                    artist, created = Artist.objects.get_or_create(
+                        id=artist_id,
+                        defaults={
+                            'name': artist_info['name'],
+                            'photo_url': artist_photo_url,
+                            'genres': artist_info['genres'],
+                            'popularity': artist_info['popularity'],
+                        }
+                    )
+
+                    for album in sorted_albums:
+                        Album.objects.get_or_create(
+                            id=album['id'],
+                            defaults={
+                                'name': album['name'],
+                                'album_type': album['album_type'],
+                                'release_date': album['release_date'],
+                                'image_url': album['images'][0]['url'] if album['images'] else None,
+                                'artist': artist,
+                            }
+                        )
 
         except Exception as e:
             error = str(e)
