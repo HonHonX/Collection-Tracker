20ea7ad53e784ebf817b78e82061805b9ff862b6
Sherry <119943082+HonHonX@users.noreply.github.com>
2025-01-05 03:56:08 +0100

    further update dashboard

--- collectionTracker/stats/views.py
+++ collectionTracker/stats/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from collection.models import UserProgress, Genre, UserAlbumCollection
+from collection.models import UserProgress, Genre, UserAlbumCollection, Artist
 from django.db.models import Count, Q
 
 @login_required
@@ -12,7 +12,6 @@ def dashboard_view(request):
         user_progress = UserProgress.objects.create(user=user)
 
     # Calculate top 5 genres based on the number of albums in the user's collection
-
     top_genres = Genre.objects.annotate(
         album_count=Count(
             'artists__album__useralbumcollection',
@@ -20,15 +19,22 @@ def dashboard_view(request):
         )
     ).filter(album_count__gt=0).order_by('-album_count')[:5]
 
-
-
     # Add album IDs for each genre
     for genre in top_genres:
         genre.album_ids = genre.get_album_ids()
 
+    # Calculate top 10 artists based on the number of albums in the user's collection
+    top_artists = Artist.objects.annotate(
+        album_count=Count(
+            'album__useralbumcollection',
+            filter=Q(album__useralbumcollection__user=user)
+        )
+    ).filter(album_count__gt=0).order_by('-album_count')[:10]
+
     context = {
         'user_progress': user_progress,
         'top_genres': top_genres,
+        'top_artists': top_artists,
     }
     return render(request, 'stats/dashboard.html', context)
 
